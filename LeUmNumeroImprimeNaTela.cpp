8dc3e0f3f3c841ea694617b0d93a9ab442efb88a
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-05-19 20:18:51 -0300

    Lê um numero digitado e Impreme o valor na tela

---
+++ LeUmNumeroImprimeNaTela.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int x;
+int main()
+{
+    printf("    Informe um Numero ");
+    scanf("%i",&x);
+    printf("    O numero que vc digitou eh = %i \n\n\n",x);
+    system("pause");
+    return 0;
+}
