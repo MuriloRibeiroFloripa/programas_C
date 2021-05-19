cedafc319870cc3942a37e987e19f9d5bde75d53
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-05-19 20:40:23 -0300

    Lê um valor e apresenta ele ao quadrado

---
+++ apresentaQuadradoDoValor.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int x,y;
+    printf("    Informe um Numero ");
+    scanf("%i",&x);
+    
+    printf("    O Quadrado do numero que vc digitou eh = %i \n\n\n",x*x);
+    system("pause");
+    return 0;
+}
