ec9882479ec5f04489ce0c0bc0cda35832a96441
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-05-20 07:41:35 -0300

    Soma dois Valores e apresenta o resultado

---
+++ somaDoisValores.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+	int x,y,total;
+	
+    printf("    Informe um Numero x ");
+    scanf("%i",&x);
+    
+    printf("    Informe um Numero y ");
+    scanf("%i",&y);
+    
+    total=x+y;
+    
+    printf("    A soma dos Valores eh = %i \n\n\n",total);
+   
+    system("pause");
+    return 0;
+}
