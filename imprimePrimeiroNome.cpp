c4fd16bd5217979dd232f9775142a0ab05a50bbe
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-05-21 10:27:56 -0300

    Imprimi o primeiro nome

---
+++ imprimePrimeiroNome.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+   
+    char a[50];
+   
+    printf("\n\n     Ecreva seu nome:    \n\n\n");
+    scanf("%s",&a);
+    
+    printf("\n\n %s \n\n\n",a);   
+    
+    system("pause");
+	return 0;
+}
